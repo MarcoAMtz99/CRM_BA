29bdd2846fb897389de2def4e4b3dcc60e0b3f41
MarcoAMtzMtz <marcoamtz99@gmail.com>
2023-09-11 14:29:32 -0600

    Link cifrado test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,36 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function encryptJson()
+    {
+        $json ='{
+           "clienteUnico":{
+              "idPais":1,
+              "idCanal":1,
+              "idSucursal":115,
+              "folio":1969
+           },
+           "numeroTelefono":"559840744",
+           "idCampana":1738,
+           "numeroEmpleado":"800046",
+           "token":"Cobranza2022",
+           "idOrigen":2,
+           "idDespacho":1,
+           "fecha":"19/12/2022 13:20:00"
+        }';
+
+        $key = "192cY7vUQbodWq4q";
+
+        $iv = "192cY7vUQbodWq4q";
+
+        $cipherText = openssl_encrypt($json, 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv);
+
+        $cipherTextBase64 = base64_encode($cipherText);
+
+        dd("https://www.gestioncobranzabaz.com.mx/regional/front-gestiones/index.html#/front-cobranza/credimax/".$cipherTextBase64);
+
+        // return response()->json(['cipherText' => $cipherTextBase64]);
+    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/campañas', [App\Http\Controllers\CampaignController::class, 'index
 Route::get('/campañas/detalle/{id}', [App\Http\Controllers\CampaignController::class, 'getCampaign'])->name('getCampaign');
 Route::get('/consulta', [App\Http\Controllers\ManagerController::class, 'index'])->name('manager');
 
+
 Route::get('/test', function () {
     return view('test');
 });
 });
+
+Route::get('/link', [App\Http\Controllers\HomeController::class, 'encryptJson'])->name('link');
+
