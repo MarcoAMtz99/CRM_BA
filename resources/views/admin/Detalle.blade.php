671624cc865c31721d18e4e0a6c00dc973a25237
MarcoAMtzMtz <marcoamtz99@gmail.com>
2023-09-20 10:34:29 -0600

    Fix

--- resources/views/admin/Detalle.blade.php
+++ resources/views/admin/Detalle.blade.php
@@ -7,7 +7,7 @@
             @php
             $id = request()->route('id');
             @endphp 
-            <h2>{{$employeeNumber}}</h2>
+            <h2>{{$employee}}</h2>
             <detail-component 
             :id="{{ $id }}"
             :numero="{{ $employee }}"></detail-component>
