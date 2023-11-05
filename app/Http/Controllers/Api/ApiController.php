cebc8cb343064492090e24bac3e1018ae047f750
MarcoAMtzMtz <marcoamtz99@gmail.com>
2023-11-05 12:34:49 -0600

    Api response

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -27,9 +27,8 @@ class ApiController extends Controller
             $response = Http::withHeaders($headers)->post($url);
             $data = $response->json();
 
-            return response()->json(['Data' => $data], 200);
-            // return $data;
-            // Verifica que se haya obtenido una respuesta válida
+            // return response()->json(['Data' => $data], 200);
+            
             if ($response->successful()) {
                 $campanias = $data['resultado']['campanias'] ?? [];
 
@@ -38,13 +37,10 @@ class ApiController extends Controller
                     return $camp['id'];
                 }, $campanias);
 
-                // Llama a la función para consultar el endpoint B con los IDs
-                $resultados = $this->consultarEndpointB($ids);
+                $resultados = $this->consultClients($ids);
 
-                // Haz lo que necesites con los resultados
                 return response()->json(['resultados' => $resultados]);
             } else {
-                // Maneja el caso de una respuesta no exitosa según tus necesidades
                 return response()->json(['error' => 'No se pudo obtener datos de la API'], 500);
             }
         } catch (\Exception $e) {
@@ -52,18 +48,85 @@ class ApiController extends Controller
         }
     }
 
-    private function consultarEndpointB($ids)
+    public function consultClients($idCampanas)
     {
         $resultados = [];
 
-        foreach ($ids as $id) {
-            // Realiza la solicitud a endpoint B y agrega los resultados al array
-            $response = Http::get("https://tu-api-endpoint-b/{$id}");
-            if ($response->successful()) {
-                $resultados[] = $response->json();
-            }
-        }
+        try {
+            $idDespacho = 15;
+            $headers = [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Basic ' . base64_encode('JeYXUErLkDgWzA9Pp8c2uMOkSppDq9YafWQzXVkv6itpvVrGejXSf:kDF3NFAPwKyPU8JS+Q25nAb0Fn66/RrtlJ3kofnJ8VOcbiraA0nU6w'),
+                'x-api-key' => 'SDRgX_Jv8vziBFIQHSCNcCIkc6pmJHlPcDUFl8lWajg=',
+            ];
 
-        return $resultados;
+            foreach ($idCampanas as $idCampana) {
+                $url = "https://www.gestioncobranzabaz.com.mx/GestionesCC/v2/consulta-clientes?idDespacho={$idDespacho}&idCampana={$idCampana}";
+                $response = Http::withHeaders($headers)->post($url);
+                $data = $response->json();
+
+                if ($response->successful() && isset($data['resultado']['clientes']) && is_array($data['resultado']['clientes'])) {
+                    foreach ($data['resultado']['clientes'] as $item) {
+                        // Limita la lista de teléfonos a tres si es que vienen
+                        $telefonos = [];
+                        if (isset($item['telefonos']) && is_array($item['telefonos'])) {
+                            for ($i = 0; $i < min(count($item['telefonos']), 3); $i++) {
+                                $telefonos[] = $item['telefonos'][$i]['numero'];
+                            }
+                        }
+
+                        $telefono1 = isset($telefonos[0]) ? $telefonos[0] : null;
+                        $telefono2 = isset($telefonos[1]) ? $telefonos[1] : null;
+                        $telefono3 = isset($telefonos[2]) ? $telefonos[2] : null;
+
+                        $resultados[] = [
+                            'idCampania' => $item['idCampania'],
+                            'idPais' => $item['idPais'],
+                            'idCanal' => $item['idCanal'],
+                            'idSucursal' => $item['idSucursal'],
+                            'folio' => $item['folio'],
+                            'semanasAtraso' => $item['semanasAtraso'],
+                            'saldo' => $item['saldo'],
+                            'saldoCapital' => $item['saldoCapital'],
+                            'pagoRequerido' => $item['pagoRequerido'],
+                            'nombre' => $item['nombre'],
+                            'apellidoPaterno' => $item['apellidoPaterno'],
+                            'apellidoMaterno' => $item['apellidoMaterno'],
+                            'telefono1' => $telefono1,
+                            'telefono2' => $telefono2,
+                            'telefono3' => $telefono3,
+                            'correos' => isset($item['correos']) && is_array($item['correos']) ? $item['correos'] : [],
+                            'diaPago' => $item['diaPago'],
+                            'diasAtraso' => $item['diasAtraso'],
+                            'pagoMinimo' => $item['pagoMinimo'],
+                            'pagoNoGeneraIntereses' => $item['pagoNoGeneraIntereses'],
+                            'pagoDisponible' => $item['pagoDisponible'],
+                            'abonoPuntual' => $item['abonoPuntual'],
+                            'abonoSemanal' => $item['abonoSemanal'],
+                            'capacidadPago' => $item['capacidadPago'],
+                            'creditoActivo' => $item['creditoActivo'],
+                            'fechaProximaPago' => $item['fechaProximaPago'],
+                            'fechaVencimiento' => $item['fechaVencimiento'],
+                            'creditoAutorizado' => $item['creditoAutorizado'],
+                            'tasaInteres' => $item['tasaInteres'],
+                            'calle' => $item['calle'],
+                            'numeroInterior' => $item['numeroInterior'],
+                            'numeroExterior' => $item['numeroExterior'],
+                            'colonia' => $item['colonia'],
+                            'codigoPostal' => $item['codigoPostal'],
+                            // Agrega otros atributos según sea necesario
+                        ];
+                    }
+                } else {
+                    // Puedes manejar el error de la solicitud, por ejemplo, registrándolo o tomando alguna otra acción necesaria.
+                }
+            }
+
+            return response()->json(['resultados' => $resultados]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
+
+
 }
