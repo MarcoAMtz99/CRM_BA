06d6296a45ee4fe88e8bf2c76e1c7ef541896e9f
MarcoAMtzMtz <marcoamtz99@gmail.com>
2023-11-05 12:01:42 -0600

    Api response

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+
+class ApiController extends Controller
+{
+    //
+    public function consultarAPI()
+    {
+        try {
+            $response = Http::get('https://www.gestioncobranzabaz.com.mx/GestionesCC/v1/consulta-campania', [
+                'idDespacho' => 15,
+            ]);
+
+            $data = $response->json();
+            return $data;
+            // Verifica que se haya obtenido una respuesta válida
+            if ($response->successful()) {
+                $campanias = $data['resultado']['campanias'] ?? [];
+
+                // Obtén los IDs de las campañas
+                $ids = array_map(function ($camp) {
+                    return $camp['id'];
+                }, $campanias);
+
+                // Llama a la función para consultar el endpoint B con los IDs
+                $resultados = $this->consultarEndpointB($ids);
+
+                // Haz lo que necesites con los resultados
+                return response()->json(['resultados' => $resultados]);
+            } else {
+                // Maneja el caso de una respuesta no exitosa según tus necesidades
+                return response()->json(['error' => 'No se pudo obtener datos de la API'], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    private function consultarEndpointB($ids)
+    {
+        $resultados = [];
+
+        foreach ($ids as $id) {
+            // Realiza la solicitud a endpoint B y agrega los resultados al array
+            $response = Http::get("https://tu-api-endpoint-b/{$id}");
+            if ($response->successful()) {
+                $resultados[] = $response->json();
+            }
+        }
+
+        return $resultados;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\ApiController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::delete('users/{id}', [UserController::class,'destroy']); 
 Route::put('users/{id}', [UserController::class,'update']); 
+Route::get('/consultar-api', 'ApiController@consultarAPI');
