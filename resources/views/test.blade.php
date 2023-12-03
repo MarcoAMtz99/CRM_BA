7a7333cd2a55da724143b22ecde2508f805cb791
MarcoAMtzMtz <marcoamtz99@gmail.com>
2023-12-02 23:18:31 -0600

    Server timezone

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,18 +1,21 @@
-
-
-
-
-
 @extends('layouts.test')
 
 @section('content')
 
-
     <h1>Prueba test para conexiones</h1>
 
-    La hora del servidor es: {{ now()->format('Y-m-d H:i:s') }}
-<!-- <example-component></example-component> -->
+    <hr>
 
-<hr>
+    La hora del servidor es: {{ now()->format('Y-m-d H:i:s') }}
+
+    <hr>
+
+    La zona horaria actual es: {{ \Illuminate\Support\Carbon::now()->timezoneName }}
+    <hr>
+    Fecha:
+    {{\Illuminate\Support\Carbon::now()}}
+    <hr>
+    La zona horaria de Apache es: {{ $_SERVER['TZ'] ?? 'No se pudo obtener la zona horaria' }}
+    <hr>
 
 @endsection
